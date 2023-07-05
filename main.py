454e3693d419871817fb2e22e3859c6162068fca
MelvinCarlPote3399 <108502733+MelvinCarlPote3399@users.noreply.github.com>
2023-07-04 20:59:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+# Creating a Pong game replica
+
+"""
+Steps:
+
+1. Create the screen
+2. Create and move a paddle
+3. Create another paddle
+4. Create the ball and make it move
+5. Detect collision with wall and bounce
+6. Detect collision with paddle
+7. Detect when paddle misses
+8. Keep score
+
+"""
+
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+ball = Ball()
+
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with the wall and bounce
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with the paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    if scoreboard.l_score == 5 or scoreboard.r_score == 5:
+        print("Game over!")
+        game_is_on = False
+
+screen.exitonclick()
